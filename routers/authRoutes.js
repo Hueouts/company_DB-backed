31fc40eb2258800b39a0fe43482f4dde9beeded5
Hueouts <hueouts@gmail.com>
2024-09-13 18:27:22 +0500

    Update authRoutes.js

--- routers/authRoutes.js
+++ routers/authRoutes.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
-
+// authentication complete
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 router.post('/verified', authController.verifyToken);
